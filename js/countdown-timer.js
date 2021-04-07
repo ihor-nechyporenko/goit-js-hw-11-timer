291859e44ec0bd31451d858e56bd8fc6018bbb81
Ihor Nechyporenko <nechyporenko.i@gmail.com>
2021-04-07 15:01:16 +0300

    add countdown timer script & render html for clockface

---
+++ js/countdown-timer.js
@@ -0,0 +1,61 @@
+import clockFaceMarkup from './clockface-markup.js';
+
+document.querySelector('body').insertAdjacentHTML('afterbegin', clockFaceMarkup());
+
+const ref = {
+  days: document.querySelector('span[data-value="days"]'),
+  hours: document.querySelector('span[data-value="hours"]'),
+  mins: document.querySelector('span[data-value="mins"]'),
+  secs: document.querySelector('span[data-value="secs"]'),
+};
+
+class CountdownTimer {
+  constructor({ targetDate }) {
+    this.timerId = null;
+    this.targetDate = targetDate;
+  };
+
+  start() {
+  this.timerId = setInterval(() => {
+    const currentTime = Date.now();
+    const deltaTime = this.targetDate.getTime() - currentTime;
+    const { days, hours, mins, secs } = this.getTimeComponents(deltaTime);
+    if (+days === 0 && +hours === 0 && +mins === 0 && +secs === 0) {
+      stop();
+    }
+    console.log(`${days}:${hours}:${mins}:${secs}`);
+    updateClockFace({ days, hours, mins, secs });
+  }, 1000);
+  };
+
+  stop() {
+    clearInterval(this.timerId);
+  };
+
+  getTimeComponents(time) {
+    const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+    const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+
+    return { days, hours, mins, secs };
+  };
+
+  pad(value) {
+    return String(value).padStart(2, '0');
+  };
+};
+
+const timer = new CountdownTimer({
+  selector: '#timer-1',
+  targetDate: new Date('Apr 08, 2021'),
+});
+
+timer.start();
+
+function updateClockFace({ days, hours, mins, secs }) {
+  ref.days.textContent = days;
+  ref.hours.textContent = hours;
+  ref.mins.textContent = mins;
+  ref.secs.textContent = secs;
+}
